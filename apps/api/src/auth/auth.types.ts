f56fd027a9285cf7cf7a6480d48b3f6fbe527f39
Bakhtiar Ahmed <ahmedbakhtiar41@gmail.com>
2026-07-23 23:40:33 +0500

    Delete apps directory

--- apps/api/src/auth/auth.types.ts
+++
@@ -1,8 +0,0 @@
-import { UserRole } from '@prisma/client';
-
-export type AuthUser = {
-  id: string;
-  email: string;
-  role: UserRole;
-};
-
--- apps/api/tmp_migration_check.sql
+++
@@ -1,4 +0,0 @@
-select migration_name, checksum, finished_at, rolled_back_at
-from "_prisma_migrations"
-where migration_name = '20260506212413_align_quotation_statuses';
-
